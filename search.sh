210fa83a975771c5f65992af36db5e5f45ca4795
korchasa <korchasa@syneforge.com>
2019-07-02 18:51:54 +0300

    feat: search script with tmp-file

--- search.sh
+++ search.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-# http https://api.github.com/search/repositories\?q=clickhouse\&sort=stars\&order=desc\&per_page=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url' > ./github.txt
-
+http https://api.github.com/search/repositories\?q=clickhouse\&sort=stars\&order=desc\&per_page=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url' > ./tmp.txt
 echo "New repos: "
-for item in $(cat ./github.txt)
+for item in $(cat ./tmp.txt)
 do
-    if ! grep -q "${item}" ./README.adoc; then
+    if ! grep -q "${item}" ./github.txt; then
         open "${item}"
     fi
 done
