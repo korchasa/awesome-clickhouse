9e100b448417561fb7500f7b847dfdd1bd33c1d9
korchasa <korchasa@syneforge.com>
2019-07-02 18:53:17 +0300

    fix: useless line

--- search.sh
+++ search.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 http https://api.github.com/search/repositories\?q=clickhouse\&sort=stars\&order=desc\&per_page=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url' > ./tmp.txt
-echo "New repos: "
 for item in $(cat ./tmp.txt)
 do
     if ! grep -q "${item}" ./github.txt; then
