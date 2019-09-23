80b595ce234811b7686c9ec1a623c0acebc80ee1
korchasa <korchasa@gmail.com>
2019-09-23 21:52:20 +0300

    fix: search.sh without tmp file

--- search.sh
+++ search.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-http https://api.github.com/search/repositories\?q\=clickhouse\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url + " [" + .full_name + "] - " +.description' > ./tmp.txt
-while IFS= read -r item; do
+resp=$(http https://api.github.com/search/repositories\?q\=clickhouse\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url + " [" + .full_name + "] - " +.description')
+while read -r item; do
     url=$(sed "s/ .*$//" <<< $item)
     if ! grep -q "${url}" ./README.adoc; then
         echo ${item}
     fi
-done < "./tmp.txt"
+done <<< "${resp}"
