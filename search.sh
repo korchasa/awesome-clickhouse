62738abde16b7b5f401a3a83cbab2fa6e4483d04
korchasa <korchasa@gmail.com>
2019-09-23 21:44:32 +0300

    feat: improve search.sh

--- search.sh
+++ search.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-http https://api.github.com/search/repositories\?q=clickhouse\&sort=stars\&order=desc\&per_page=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url' > ./tmp.txt
-for item in $(cat ./tmp.txt)
-do
-    if ! grep -q "${item}" ./github.txt; then
-        open "${item}"
+http https://api.github.com/search/repositories\?q\=clickhouse\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url + " [" + .full_name + "] - " +.description' > ./tmp.txt
+while IFS= read -r item; do
+    url=$(sed "s/ .*$//" <<< $item)
+    if ! grep -q "${url}" ./README.adoc; then
+        echo ${item}
     fi
-done
+done < "./tmp.txt"
