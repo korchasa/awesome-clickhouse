4a33997a4a26436f46561f97fddfa5d657bb9118
korchasa <66022+korchasa@users.noreply.github.com>
2021-04-11 16:48:36 +0300

    feat: lower stars count in search

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 date=$(date -v -3m '+%Y-%m-%d')
-resp=$(http https://api.github.com/search/repositories\?q\=clickhouse+pushed:\>$date\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 50) | .html_url + " [" + .full_name + "] - " +.description')
+resp=$(http https://api.github.com/search/repositories\?q\=clickhouse+pushed:\>$date\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 30) | .html_url + " [" + .full_name + "] - " +.description')
 while read -r item; do
     url=$(sed "s/ .*$//" <<< $item)
     if ! grep -q "${url}" ./README.adoc; then
