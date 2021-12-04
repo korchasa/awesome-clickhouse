5ff98ee24179b724ea32cfbf03fab3041c0713e5
korchasa <korchasa@icloud.com>
2021-12-04 14:44:24 +0200

    fix: sed: RE error: illegal byte sequence

--- search.sh
+++ search.sh
@@ -2,7 +2,7 @@
 date=$(date -v -3m '+%Y-%m-%d')
 resp=$(http https://api.github.com/search/repositories\?q\=clickhouse+pushed:\>$date\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 30) | .html_url + " [" + .full_name + "] - " +.description')
 while read -r item; do
-    url=$(sed "s/ .*$//" <<< $item)
+    url=$(LC_ALL=C sed "s/ .*$//" <<< $item)
     if ! grep -q "${url}" ./README.adoc; then
         if ! grep -q "${url}" ./ignore; then
             echo ${item}
